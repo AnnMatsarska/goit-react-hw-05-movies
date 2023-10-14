81d84fc766e2c22fd41dbd204a1d3c07568f1bc1
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-10-14 21:25:07 +0000

    Deploying to gh-pages from @ AnnMatsarska/goit-react-hw-05-movies@0ff4b30cf89e0f4e906b723c3557e5c4beb7b15e 🚀

---
+++ static/js/186.dfbb3207.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{186:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("div",{children:"Reviews"})})}}}]);
+//# sourceMappingURL=186.dfbb3207.chunk.js.map
---
+++ static/js/278.16604d2e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[278],{278:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("div",{children:"Movies"})})}}}]);
+//# sourceMappingURL=278.16604d2e.chunk.js.map
