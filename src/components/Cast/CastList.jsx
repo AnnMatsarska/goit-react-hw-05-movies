df5f5e8824f594a7777a4f9527de08d7aaca01fe
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-10-16 23:33:09 +0300

    Update CastList.jsx

--- src/components/Cast/CastList.jsx
+++ src/components/Cast/CastList.jsx
@@ -5,23 +5,25 @@ export const CastList = ({ cast }) => {
   const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w200/';
   return (
     <List>
-      {cast.map(({ profile_path, original_name, character, id }) => (
-        <li key={id}>
-          <Img
-            src={profile_path ? IMAGE_BASE_URL + profile_path : noImage}
-            alt={original_name}
-            width="200px"
-            height="300px"
-            loading="lazy"
-          />
-          <CastInfoWrapper>
-            <CastName>{original_name}</CastName>
-            <p>
-              <b>Character:</b> {character}
-            </p>
-          </CastInfoWrapper>
-        </li>
-      ))}
+      {cast.length > 0
+        ? cast.map(({ profile_path, original_name, character, id }) => (
+            <li key={id}>
+              <Img
+                src={profile_path ? IMAGE_BASE_URL + profile_path : noImage}
+                alt={original_name}
+                width="200px"
+                height="300px"
+                loading="lazy"
+              />
+              <CastInfoWrapper>
+                <CastName>{original_name}</CastName>
+                <p>
+                  <b>Character:</b> {character}
+                </p>
+              </CastInfoWrapper>
+            </li>
+          ))
+        : 'Unfortunately, there are no actors for this movie.'}
     </List>
   );
 };
