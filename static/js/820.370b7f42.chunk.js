784431656c7155e6a4755dfeac026027a630ed66
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-10-16 14:53:58 +0000

    Deploying to gh-pages from @ AnnMatsarska/goit-react-hw-05-movies@9cbaf9cf5cf8f006468f279a3ff4a6f8e24711ac 🚀

---
+++ static/js/820.370b7f42.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[820],{8820:function(t,e,c){c.d(e,{kyg:function(){return r}});var a=c(9983);function r(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 487.8a31.96 31.96 0 0 0 0 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z"}}]})(t)}}}]);
+//# sourceMappingURL=820.370b7f42.chunk.js.map
